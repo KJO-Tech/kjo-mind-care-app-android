1de38b77e658b7b01ecb787dadd54b91f4380d32
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2025-07-01 16:19:18 -0500

    fix: fix scroll when keyboard is displayed

--- app/src/main/java/tech/kjo/kjo_mind_care/ui/main/blog_form/BlogFormScreen.kt
+++ app/src/main/java/tech/kjo/kjo_mind_care/ui/main/blog_form/BlogFormScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -147,6 +148,7 @@ fun BlogFormScreen(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(paddingValues)
+                    .imePadding()
                     .padding(16.dp)
                     .verticalScroll(rememberScrollState()),
                 horizontalAlignment = Alignment.CenterHorizontally
